e5f29767ef2b21e36b21e70347c010795715eeba
unknown <vietnh1295@gmail.com>
2019-05-29 09:39:28 +0700

    add index.html,myscript.js

---
+++ myscript.js
@@ -0,0 +1,15 @@
+function showMessage(){
+    alert('Xin chào!');
+}
+function showMessagebyHtml()
+{
+    document.getelementbyid('messageDiv').innerHtml='Xin chào!';
+}
+function showMessagebyDocument()
+{
+    document.write('Xin chào');
+}
+function showMessagebyConsole()
+{
+    Console.log('Xin chào');
+}
